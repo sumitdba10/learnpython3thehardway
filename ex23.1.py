f3c75b14f115d979da01c22ee371077be1e74880
sumitdba10 <32470693+sumitdba10@users.noreply.github.com>
2019-08-22 09:27:24 -0400

    added utf-8 encoding program

---
+++ ex23.1.py
@@ -0,0 +1,26 @@
+import sys
+
+script, encoding, errors = sys.argv
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+
+
+def print_line(line, encoding, errors):
+    new_lang = line.strip()
+    raw_bytes = new_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding,errors=errors)
+
+
+    print(raw_bytes, '<=======>',cooked_string)
+
+
+language = open("languages.txt",encoding="utf-8")
+
+
+main(language,encoding, errors)
